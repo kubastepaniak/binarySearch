3ab69ecc453dafd4ff72b6588240fcd8e5f082ca
Jakub Stepaniak <stepaniak.kuba@gmail.com>
2018-06-05 21:29:14 +0200

    init update

---
+++ bsearch.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void *bsearch(const void *key, const void *base,
+			  size_t nmemb, size_t size,
+			  int (*compar)(const void *, const void *));
+
+int comparison(const void *a, const void *b);
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
